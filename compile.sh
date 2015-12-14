88c2e0ee31e8dc7f21b83aa783a2c7c56d978b0a
Fix vm state in balancer <disanzo@dis.uniroma1.it>
2015-12-14 17:02:08 +0100

    first commit

---
+++ compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd rapl-power
+make clean
+make
+cd ..
+cd tinySTM
+make clean
+make
+cd ..
+cd stamp/intruder
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd yada
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd vacation
+make -f Makefile.stm clean
+make -f Makefile.stm
+
+
