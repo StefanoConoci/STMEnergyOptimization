8d11933ab2a5ab98a711e0f5bbc8c65b408a22a5
io <disanzo@dis.uniroma1.it>
2016-01-22 19:21:17 +0100

    a

--- stamp/genome/sequencer.c
+++ stamp/genome/sequencer.c
@@ -300,7 +300,7 @@ sequencer_run (void* argPtr)
         }
         TM_END();
     }
-
+    stm_ungate_thread();
     thread_barrier_wait();
 
     /*
@@ -414,6 +414,7 @@ sequencer_run (void* argPtr)
         }
     }
 
+    stm_ungate_thread();
     thread_barrier_wait();
 
     /*
@@ -519,7 +520,7 @@ sequencer_run (void* argPtr)
             } /* iterate over chain */
 
         } /* for (endIndex < numUniqueSegment) */
-
+        stm_ungate_thread();
         thread_barrier_wait();
 
         /*
@@ -558,12 +559,12 @@ sequencer_run (void* argPtr)
                 endInfoEntries[j].jumpToNext = i - j;
             }
         }
-
+        stm_ungate_thread();
         thread_barrier_wait();
 
     } /* for (substringLength > 0) */
 
-
+    stm_ungate_thread();
     thread_barrier_wait();
 
     /*
