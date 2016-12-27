ace0eb9f745a8779498e5666dd2721f930f6122a
Stefano <stefano@Stefano-Desktop.lan>
2016-12-27 13:31:46 +0100

    Server test added

--- compile.sh
+++ compile.sh
@@ -11,9 +11,36 @@ cd tinySTM
 make clean
 make
 cd ..
-cd stamp/vacation
+cd stamp/bayes
 make -f Makefile.stm clean
 make -f Makefile.stm
-
+cd ..
+cd genome
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd intruder
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd kmeans
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd labyrinth
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd vacation
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd ssca2
+make -f Makefile.stm clean
+make -f Makefile.stm
+cd ..
+cd yada
+make -f Makefile.stm clean
+make -f Makefile.stm
 
 
