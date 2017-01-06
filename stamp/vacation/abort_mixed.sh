caa9cecdcfb5234648460e21675d04edb7bc8488
Stefano Conoci <conoci@m4015.contabo.host>
2017-01-06 12:46:22 +0100

    Added NUMA test results

--- stamp/vacation/abort_mixed.sh
+++ stamp/vacation/abort_mixed.sh
@@ -4,11 +4,11 @@ for i in $(seq 1 20)
 do
 	/home/conoci/scripts/./set_p1.sh
 	/home/conoci/scripts/./set_pkg0_p11.sh
-	./vacation -n4 -q60 -u90 -r1048576 -t4194304 -c20 fullP11-mixed.txt
+	./vacation -n4 -q60 -u90 -r1048576 -t4194304 -c20 >> full_P11_mixed.txt
 	
 	/home/conoci/scripts/./set_p1.sh
-	/home/conoci/scripts/./set_pkg0_half_p11.sh >> halfP11-mixed.txt
-	./vacation -n4 -q60 -u90 -r1048576 -t4194304 -c20	
+	/home/conoci/scripts/./set_pkg0_half_p11.sh 
+	./vacation -n4 -q60 -u90 -r1048576 -t4194304 -c20 >> half_P11_mixed.txt	
 	
 	echo "Iteration $i completed"
 
