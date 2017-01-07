4b15d136986df8aa77f7cd575c84f63b8e09822f
Stefano <stefano@Stefano-Desktop.lan>
2017-01-07 15:35:37 +0100

    minor

--- steps_test.sh
+++ steps_test.sh
@@ -6,40 +6,40 @@ do
 	echo "Iteration $i"
 	
 	./set_profile 5 80 2500 20 0 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-0.txt
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-0.txt
 
 	./set_profile 5 80 2500 20 1 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-1.txt
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-1.txt
 	
 	./set_profile 5 80 2500 20 2 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-2.txt 
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-2.txt 
 
 	./set_profile 5 80 2500 20 3 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-3.txt 
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-3.txt 
 	
 	./set_profile 5 80 2500 25 4 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-4.txt 
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-4.txt 
 	
 	./set_profile 5 80 2500 25 5 15
-	sudo stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-5.txt
+	stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruder-5.txt
 	
 	./set_profile 5 80 10000 60 0 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-0.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-0.txt
 	
 	./set_profile 5 80 10000 60 1 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-1.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-1.txt
 
 	./set_profile 5 80 10000 60 2 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-2.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-2.txt
 
 	./set_profile 5 80 10000 60 3 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-3.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-3.txt
 
 	./set_profile 5 80 10000 60 4 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-4.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-4.txt
 	
 	./set_profile 5 80 10000 60 5 15 
-	sudo stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-5.txt
+	stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t41943040 -c20 >> bench/Server/steps/vacation-5.txt
 
 done 
 
