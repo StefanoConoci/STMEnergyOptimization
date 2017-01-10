79cf7cf5cad5eacde959f54db7ae3a5d762c9933
Stefano <stefano@Stefano-Desktop.lan>
2017-01-10 19:25:32 +0100

    Added new tst

---
+++ steps_test2.sh
@@ -0,0 +1,39 @@
+ITERATIONS=10
+export STM_STATS=1
+
+for i in $(seq 1 $ITERATIONS)
+do
+	echo "Iteration $i"
+	
+	./set_profile 5 80 2000 20 0 15
+	numactl --physcpubind=+0-9 stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruderbound-0.txt
+
+	./set_profile 5 80 2000 20 1 15
+	numactl --physcpubind=+0-9 stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruderbound-1.txt
+	
+	./set_profile 5 80 2000 20 2 15
+	numactl --physcpubind=+0-9 stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruderbound-2.txt 
+
+	./set_profile 5 80 2000 20 3 15
+	numactl --physcpubind=+0-9 stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruderbound-3.txt 
+	
+	./set_profile 5 80 2000 25 7 15
+	numactl --physcpubind=+0-9 stamp/intruder/./intruder -a10 -l128 -n262144 -s1 -t20 >> bench/Server/steps/intruderbound-7.txt
+	
+	./set_profile 5 80 10000 60 0 15 
+	stamp/yada/./yada -a30 -i stamp/yada/inputs/ttimeu1000000.2 -t20 >> bench/Server/steps/yada-0.txt
+	
+	./set_profile 5 80 10000 60 1 15 
+	stamp/yada/./yada -a30 -i stamp/yada/inputs/ttimeu1000000.2 -t20 >> bench/Server/steps/yada-1.txt
+
+	./set_profile 5 80 10000 60 2 15 
+	stamp/yada/./yada -a30 -i stamp/yada/inputs/ttimeu1000000.2 -t20 >> bench/Server/steps/yada-2.txt
+
+	./set_profile 5 80 10000 60 3 15 
+	stamp/yada/./yada -a30 -i stamp/yada/inputs/ttimeu1000000.2 -t20 -c20 >> bench/Server/steps/yada-3.txt
+
+	./set_profile 5 80 10000 60 7 15 
+	stamp/yada/./yada -a30 -i stamp/yada/inputs/ttimeu1000000.2 -t20 -c20 >> bench/Server/steps/yada-7.txt
+
+done 
+
