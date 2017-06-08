55dfb85d28afc503978e5e1e50186b3f991e37f7
StefanoConoci <stefano.conoci@gmail.com>
2017-06-08 11:03:43 +0200

    minor

--- tinySTM/src/heuristics.c
+++ tinySTM/src/heuristics.c
@@ -1136,6 +1136,7 @@ void dynamic_heuristic1(double throughput, double  abort_rate, double power, dou
 						current_window_slot = 0;
 
 						#ifdef DEBUG_HEURISTICS
+							printf(" - Next configuration = BEST (restart)\n");
 							printf("BEST - threads %d - pstate %d - power %lf\n", best_threads, best_pstate, best_power);
 							printf("HIGH - threads %d - pstate %d - power %lf\n", high_threads, high_pstate, high_power);
 							printf("LOW - threads %d - pstate %d - power %lf\n", low_threads, low_pstate, low_power);
