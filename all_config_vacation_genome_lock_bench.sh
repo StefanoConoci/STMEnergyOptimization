52d314baa3c6b04f218d4ab7d355167efbebbf1e
Stefano Conoci <stefano.conoci@gmail.com>
2017-06-12 23:02:01 +0200

    minor

---
+++ all_config_vacation_genome_lock_bench.sh
@@ -0,0 +1,21 @@
+ITERATIONS=15
+export STM_STATS=1
+
+for i in $(seq 0 11)
+do
+	for j in $(seq 1 20)
+	do
+		/home/conoci/git/STMEnergyOptimization/./set_profile $j $i 80.0 10000 50 8 10 0 10 10 100 10.0 10 1.0
+		for b in $(seq 1 $ITERATIONS)	
+		do	
+			echo "P-state=$i"
+			echo  "Threads=$j"
+	        echo "Iteration $b"
+			echo ""
+			numactl --physcpubind=+0-19 /home/conoci/git/STMEnergyOptimization/stamp/vacation/./vacation -n4 -q60 -u90 -r1048576 -t119430 -c$j >> bench/SBAC-PAD/results/all_config_lock/vacation-$i-$j.txt
+			numactl --physcpubind=+0-19 /home/conoci/git/STMEnergyOptimization/stamp/genome/./genome -g26384 -s64 -n16777216 -t$j >> bench/SBAC-PAD/results/all_config_lock/genome-$i-$j.txt
+		done
+	done 
+done
+
+echo "Benchmark completed."
