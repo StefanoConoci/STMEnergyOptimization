a37faaf6d5085c942af89e107793eecf9a68ef8b
StefanoConoci <stefano.conoci@gmail.com>
2017-08-26 13:20:54 +0200

    Added experimental results aand updated readme for PPoPP submission

---
+++ bench/PPoPP/results/stm_dualphase_fixed_merged/convert_gnuplot.sh
@@ -0,0 +1,8 @@
+./tp heuristics-50-stm.csv >> plotdata/tp-50-stm.txt
+./error heuristics-50-stm.csv >> plotdata/error-50-stm.txt
+
+./tp heuristics-60-stm.csv >> plotdata/tp-60-stm.txt
+./error heuristics-60-stm.csv >> plotdata/error-60-stm.txt
+
+./tp heuristics-70-stm.csv >> plotdata/tp-70-stm.txt
+./error heuristics-70-stm.csv >> plotdata/error-70-stm.txt
